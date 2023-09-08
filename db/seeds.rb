815a206a91f53038dff41b50d2e78505a41346fe
Ra Ko <you@example.com>
2023-09-08 13:05:15 +0200

    add initial values for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,62 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+categories = Category.create([
+  { category_name: "Naturwissenschaften", category_description: "Bücher über Naturwissenschaften" },
+  { category_name: "Informationstechnologie", category_description: "Bücher über Informationstechnologien" },
+  { category_name: "Mathematik", category_description: "Bücher über Mathematik" }
+])
+
+subcategories = Subcategory.create([
+  { subcategory_topic: "Physik", subcategory_description: "Lehr-/ Sachbücher über Physik", category: categories[0] },
+  { subcategory_topic: "Chemie", subcategory_description: "Lehr-/ Sachbücher über Chemie", category: categories[0] },
+  { subcategory_topic: "Algorithmen", subcategory_description: "Lehr-/ Sachbücher über Algorithmen", category: categories[1] },
+  { subcategory_topic: "Netzwerktechnik", subcategory_description: "Lehr-/ Sachbücher über Netzwerktechnik", category: categories[1] },
+  { subcategory_topic: "Analysis", subcategory_description: "Lehr-/ Sachbücher über Mathematik über Analysis", category: categories[2] },
+  { subcategory_topic: "Lineare Algebra", subcategory_description: "Lehr-/ Sachbücher über Lineare Algebra", category: categories[2] }
+])
+
+authors = Author.create([
+  { author_name: "Giancoli" },
+  { author_name: "Tipler" },
+  { author_name: "Peter Atkins" },
+  { author_name: "Paula Bruice" },
+  { author_name: "Steven Skiena" },
+  { author_name: "Thomas Cormen" },
+  { author_name: "Anatol Badach" },
+  { author_name: "Steffen Wendzel" },
+  { author_name: "George Thomas" },
+  { author_name: "Lars Grüne" },
+  { author_name: "Michael Ruhrländer" },
+  { author_name: "Stefan Waldmann" }
+])
+
+books = Book.create([
+  { book_title: "Physik", book_isbn: "3868943633", book_description: "Lehrbuch Physik", book_price: 90, subcategory_id: subcategories[0].id },
+  { book_title: "Physik", book_isbn: "3662679353", book_description: "Lehrbuch Physik", book_price: 95, subcategory_id: subcategories[0].id },
+  { book_title: "Physikalische Chemie", book_isbn: "3527345507", book_description: "Lehrbuch Physikalische Chemie", book_price: 70, subcategory_id: subcategories[1].id },
+  { book_title: "Organische Chemie", book_isbn: "3868943412", book_description: "Lehrbuch Organische Chemie", book_price: 100, subcategory_id: subcategories[1].id },
+  { book_title: "The Algorithm Design Manual", book_isbn: "3030542556", book_description: "Lehrbuch Algorithmen", book_price: 80, subcategory_id: subcategories[2].id },
+  { book_title: "Algorithmen", book_isbn: "3486748610", book_description: "Lehrbuch Algorithmen", book_price: 105, subcategory_id: subcategories[2].id },
+  { book_title: "Technik der IP-Netze", book_isbn: "3446462104", book_description: "Lehrbuch Netzwerktechnik", book_price: 60, subcategory_id: subcategories[3].id },
+  { book_title: "IT-Sicherheit für TCP/IP- und IoT-Netzwerke", book_isbn: "9783658226022", book_description: "Lehrbuch IT-Sicherheit", book_price: 20, subcategory_id: subcategories[3].id },
+  { book_title: "Analysis 1", book_isbn: "3868941703", book_description: "Lehrbuch Analysis", book_price: 50, subcategory_id: subcategories[4].id },
+  { book_title: "Gewöhnliche Differentialgleichungen", book_isbn: "9783658102401", book_description: "Lehrbuch Mathematik", book_price: 30, subcategory_id: subcategories[4].id },
+  { book_title: "Lineare Algebra", book_isbn: "3868942718", book_description: "Lehrbuch Lineare Algebra", book_price: 35, subcategory_id: subcategories[5].id },
+  { book_title: "Lineare Algebra 1", book_isbn: "3662632624", book_description: "Lehrbuch Lineare Algebra", book_price: 35, subcategory_id: subcategories[5].id }
+])
+
+AuthorBook.create([
+  { author: authors[0], book: books[0] },
+  { author: authors[1], book: books[1] },
+  { author: authors[2], book: books[2] },
+  { author: authors[3], book: books[3] },
+  { author: authors[4], book: books[4] },
+  { author: authors[5], book: books[5] },
+  { author: authors[6], book: books[6] },
+  { author: authors[7], book: books[7] },
+  { author: authors[8], book: books[8] },
+  { author: authors[9], book: books[9] },
+  { author: authors[10], book: books[10] },
+  { author: authors[11], book: books[11] }
+])
